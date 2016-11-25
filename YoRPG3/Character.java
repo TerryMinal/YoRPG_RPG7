fcaafb42285cb544387af3740f1a9a6bdf4233dc
Terry Guan <terryguan513@gmail.com>
2016-11-24 20:17:28 -0500

    added evasion, origEvasion, state variables. Added methods evade, charge, and abstract defend

--- YoRPG3/Character.java
+++ YoRPG3/Character.java
@@ -2,9 +2,12 @@
 //Terry Guan, Xin Yi Chen, Alitquan Mallick
 public abstract class Character{
     protected String name; 
-    protected int health, strength, origStrength,defense, origDefense, speed, origSpeed;
+    protected int health, strength, origStrength, defense, origDefense; 
+    protected int speed, origSpeed, evasion, origEvasion;
+    protected int charge, state;  
     protected double attackRating,origAttack;
-
+    
+    //int state- 0:normal 1:posioned 2: paralyzed
         /*
       returns boolean indicating living or dead
      */
@@ -32,34 +35,50 @@ public abstract class Character{
     }
     /*
       takes an int parameter, decreases life attribute by that amount
-     */
+    */
     public void lowerHP(int damage) {
 	int origHealth = health;
 	health = origHealth - damage; 
     }
-
+    
     public void normalAttack() {
 	lowerHP(30); 
     } 
     
     public abstract void chooseAttack(); 
-
+    
     /*
       1.prepares the Warrior to perform a special attack
       2.decreases defense attribute
       3.increases attack attribute
      */
-    public abstract void specialize();
+    //determines whether a character evades an attack or not
+    public boolean evade() {
+	if ((int) (Math.random() * 100) < evasion)
+	    return true; 
+	else 
+	    return false; 
+    }
 
+    public void charge(int amount) {
+	charge += amount; 
+    }
+    
+    public abstract void specialize();
+    
     /*
       1.prepares the Warrior to perform a normal attack
       2.resets defense attribute
       3.resets attack attribute
      */
+
+    public abstract void defend(); 
+
     public void normalize(){
 	defense = origDefense; 
 	attackRating = origAttack;
-	speed = origSpeed; 
+	speed = origSpeed;
+	evasion = origEvasion; 
     }    
     
     protected abstract String about(); 
